e63b4a48ebc69a7e85c7aece5faa1d1e7f0133fd
asmirnov <smirnov141188@gmail.com>
2022-04-13 16:02:03 +0600

    PEJT-21: Added project skeleton

---
+++ src/main/java/io/codero/lettergenerator/LetterGeneratorApplication.java
@@ -0,0 +1,11 @@
+package io.codero.lettergenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterGeneratorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LetterGeneratorApplication.class, args);
+    }
+}
---
+++ src/test/java/io/codero/lettergenerator/LetterGeneratorApplicationTests.java
@@ -0,0 +1,14 @@
+package io.codero.lettergenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetterGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // TODO: make the tests
+    }
+
+}
