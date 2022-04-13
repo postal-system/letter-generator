d796bff1d78732131843e9807b2007abba3301e0
asmirnov <smirnov141188@gmail.com>
2022-04-13 17:16:10 +0600

    PEJT-21: Added send to kafka

--- src/main/java/io/codero/lettergenerator/service/impl/LetterServiceImpl.java
+++ src/main/java/io/codero/lettergenerator/service/impl/LetterServiceImpl.java
@@ -4,14 +4,22 @@ import io.codero.lettergenerator.model.Letter;
 import io.codero.lettergenerator.service.LetterService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class LetterServiceImpl implements LetterService {
+    private final KafkaTemplate<String, Letter> template;
+
+    @Value("${spring.kafka.topic}")
+    private String topic;
+
     @Override
     public void send(Letter letter) {
-        log.info("#### <- {}", letter);
+        template.send(topic, letter);
+        log.info(topic + ":#### <- {}",letter);
     }
 }
